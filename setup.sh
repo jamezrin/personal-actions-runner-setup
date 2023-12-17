7c1c773967a900c9360ca69e623df1821d94c002
Jaime Martínez Rincón <jaime@jamezrin.name>
2023-12-17 21:24:07 +0100

    feat: initial commit

---
+++ setup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+ARC_CONTROLLER_NAMESPACE="arc-systems"
+
+function install_arc_controller() {
+	helm upgrade --install arc \
+		--namespace "${ARC_CONTROLLER_NAMESPACE}" \
+		--create-namespace \
+		oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
+}
+
+ARC_RUNNER_SET_NAMESPACE="arc-runners"
+ARC_GITHUB_PAT_SECRET_NAME="arc-github-secret"
+
+function install_arc_runner_set() {
+	RELEASE_NAME=arc-$(echo "${1,,}" | sed 's/\//-/g')-set
+	GITHUB_REPO_URL="https://github.com/$1"
+	helm upgrade --install "${RELEASE_NAME}" \
+		--namespace "${ARC_RUNNER_SET_NAMESPACE}" \
+		--create-namespace \
+		--set githubConfigUrl="${GITHUB_REPO_URL}" \
+		--set githubConfigSecret="${ARC_GITHUB_PAT_SECRET_NAME}" \
+        --values gha-runner-scale-set-dind-fix.yaml \
+		oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
+}
+
+install_arc_controller
+install_arc_runner_set "jamezrin/rustplusplus-spiced"
+install_arc_runner_set "jamezrin/MonaPaste"
+install_arc_runner_set "jamezrin/timeit-webapp"
+install_arc_runner_set "jamezrin/test-spring"
+install_arc_runner_set "jamezrin/aylamusica-rails"
