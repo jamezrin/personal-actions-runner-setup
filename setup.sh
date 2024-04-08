a7c2e5a4900137472c341344cdfcfad40b41d3b1
Jaime Martínez Rincón <jaime@jamezrin.name>
2024-04-08 23:39:52 +0200

    chore: install arc at org level

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ function install_arc_controller() {
 	helm upgrade --install arc \
 		--namespace "${ARC_CONTROLLER_NAMESPACE}" \
 		--create-namespace \
-        --wait \
+		--wait \
 		oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set-controller
 }
 
@@ -15,21 +15,21 @@ ARC_GITHUB_PAT_SECRET_NAME="arc-github-secret"
 
 function install_arc_runner_set() {
 	RELEASE_NAME=arc-$(echo "${1,,}" | sed 's/\//-/g')-set
-	GITHUB_REPO_URL="https://github.com/$1"
+	GITHUB_CONFIG_URL="https://github.com/$1"
 	helm upgrade --install "${RELEASE_NAME}" \
 		--namespace "${ARC_RUNNER_SET_NAMESPACE}" \
 		--create-namespace \
-		--set githubConfigUrl="${GITHUB_REPO_URL}" \
+		--set githubConfigUrl="${GITHUB_CONFIG_URL}" \
 		--set githubConfigSecret="${ARC_GITHUB_PAT_SECRET_NAME}" \
-        --values gha-runner-scale-set-dind-fix.yaml \
-        --wait \
+		--values gha-runner-scale-set-dind-fix.yaml \
+		--wait \
 		oci://ghcr.io/actions/actions-runner-controller-charts/gha-runner-scale-set
 }
 
 install_arc_controller
-install_arc_runner_set "cyber-rust/cyber-rust"
+install_arc_runner_set "cyber-rust"
 install_arc_runner_set "jamezrin/MonaPaste"
 install_arc_runner_set "jamezrin/timeit-webapp"
 install_arc_runner_set "jamezrin/test-spring"
 install_arc_runner_set "jamezrin/aylamusica-rails"
-install_arc_runner_set "jamezrin/onthatass-auto-campaigns"
+install_arc_runner_set "jamezrin/onthebuns-auto-campaigns"
